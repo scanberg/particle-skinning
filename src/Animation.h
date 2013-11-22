caf4625ca67af72f063e6251900a525f22eee160
Robin Skånberg <scanberg@gmail.com>
2013-11-22 11:49:47 +0100

    Changes to animation... again

--- src/Animation.h
+++ src/Animation.h
@@ -22,8 +22,21 @@ class AnimationChannel
 class Animation
 {
 public:
-	Animation();
-	~Animation();
+	Animation(unsigned int channelCount, unsigned int frameCount) :
+	m_channelCount(channelCount),
+	m_frameCount(frameCount)
+	{
+		m_channel = (AnimationChannel*) malloc(sizeof(AnimationChannel) * m_channelCount);
+		for(int i=0; i<m_channelCount; ++i)
+			m_channel[i] = AnimationChannel(m_frameCount);
+	}
+
+	~Animation()
+	{
+		free(m_channel);
+	}
+
+	AnimationChannel * 	getChannel(unsigned int i) { return m_channel+i; }
 private:
 	AnimationChannel *	m_channel;
 	unsigned int		m_channelCount;
